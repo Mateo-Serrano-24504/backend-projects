c974112067bf9a1e94a783cf9f2dbd528df4b01b
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-10-14 19:56:06 +0000

    Ajusto la clase App

--- broadcast-server/src/core/app.ts
+++ broadcast-server/src/core/app.ts
@@ -1,13 +1,24 @@
 import express from "express";
-import type { Application } from "express";
+import type { Application, Router } from "express";
 
 export default class App {
     private app: Application;
+    private port: Number;
+    private baseURL: string;
+    private router: Router;
 
-    constructor() {
+    constructor(port: Number, baseURL: string, router: Router) {
         this.app = express();
+        this.port = port;
+        this.baseURL = baseURL;
+        this.router = router;
         this.configure();
     }
 
-    private configure(): void {}
+    private configure() {
+        this.app.use(this.baseURL, this.router);
+        this.app.listen(this.port, () => 
+            console.log(`App listening in port ${this.port}`)
+        );
+    }
 }
