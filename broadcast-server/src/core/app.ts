9e10afa4572828f53d48ba8373cfe020285dc575
Mateo-Serrano-24504 <mateovalentinserranogodoy@gmail.com>
2025-10-14 19:59:13 +0000

    Fix de la sintaxis en App

--- broadcast-server/src/core/app.ts
+++ broadcast-server/src/core/app.ts
@@ -3,15 +3,13 @@ import type { Application, Router } from "express";
 
 export default class App {
     private app: Application;
-    private port: Number;
-    private baseURL: string;
-    private router: Router;
 
-    constructor(port: Number, baseURL: string, router: Router) {
+    constructor(
+        private port: Number,
+        private baseURL: string,
+        private router: Router
+    ) {
         this.app = express();
-        this.port = port;
-        this.baseURL = baseURL;
-        this.router = router;
         this.configure();
     }
 
